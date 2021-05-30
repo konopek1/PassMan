712bba97ecf7956af76279b88c38daf4fd3b77ac
Radosław Kopiński <radoslaw@scythe-studio.com>
2021-05-30 23:49:43 +0200

    Commit before merge

--- app/src/main/java/com/example/passman/Interactors/VaultViewModel.kt
+++ app/src/main/java/com/example/passman/Interactors/VaultViewModel.kt
@@ -8,6 +8,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.BasicNetwork
+import com.android.volley.toolbox.DiskBasedCache
+import com.android.volley.toolbox.HurlStack
+import com.android.volley.toolbox.JsonObjectRequest
 import com.example.passman.domain.*
 import com.example.passman.presentation.vault.PasswordsData
 import com.example.passman.presentation.vault.VaultData
@@ -15,7 +22,6 @@ import com.example.passman.presentation.vault.defaultVaultData
 import javax.crypto.SecretKey
 import javax.crypto.spec.SecretKeySpec
 
-// TODO: dopisac klase do podpisywania
 
 class VaultViewModel(val activity: Activity) : ViewModel() {
 
@@ -26,19 +32,57 @@ class VaultViewModel(val activity: Activity) : ViewModel() {
 
     private val encryptedStorage = EncryptedStorage(activity)
 
+    // Instantiate the cache
+    val cache = DiskBasedCache(activity.cacheDir, 1024 * 1024) // 1MB cap
+
+    // Set up the network to use HttpURLConnection as the HTTP client.
+    val network = BasicNetwork(HurlStack())
+
+    // Instantiate the RequestQueue with the cache and network. Start the queue.
+    val requestQueue = RequestQueue(cache, network).apply {
+        start()
+    }
+
+    //todo: change to https
+    val baseUrl = "http://192.168.0.12:3000"
+
+
     init {
-        // TODO: api call -> on start fetch tha from api
+        val endpoint = "/vault/get"
+
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, baseUrl+endpoint, null,
+            { response ->
+                response.toString()
+            },
+            { error ->
+                // TODO: Handle error
+            }
+        )
+
+
+// Access the RequestQueue through your singleton class.
+        requestQueue.add(jsonObjectRequest)
+        vaults.get(0).
+
         vaults = vaults + defaultVaultData
     }
 
     fun createVault(name: String) {
+        val endpoint = "/vault/new"
         val keyPair = KeyPairGenerator().generate()
 
         Log.d("SHARE START PK: ",keyPair.publicKey)
 
         encryptedStorage.write(keyPair.publicKey,keyPair.privateKey)
-        // TODO: api call -> create new vault && vaults = vaults + new vault
-        // but don't touch private key
+
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, baseUrl+endpoint, null,
+            { response ->
+                response.toString()
+            },
+            { error ->
+                // TODO: Handle error
+            }
+        )
 
         // temp should be invoked in response to api call
         vaults = vaults + listOf(VaultData(name, keyPair.publicKey, mutableMapOf()))
